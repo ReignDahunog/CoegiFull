210ea2c520a3126d234967cadfbeb0ecc03c794a
Reignier G. Dahunog <dahunog.295426@dasmarinas.sti.edu.ph>
2025-04-16 16:30:55 +0800

    Update 1.0.6

---
+++ Assets/Scripts/MainMenuCC.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuCC : MonoBehaviour
+{
+    public void MainMenu() { 
+    SceneManager.LoadSceneAsync("Main");
+        }
+
+}
--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class DrivingTutorial : MonoBehaviour
 {
     public TextMeshProUGUI tutorialText;
-    public GameObject finishLine; // Finish Line GameObject (disabled at start)
+    public GameObject finishLine;
 
     private bool hasSteered = false;
     private bool hasBraked = false;
     private bool hasChangedCam = false;
     private bool hasTurnedOnLights = false;
 
+    private RectTransform tutorialRectTransform;
+    private Vector2 originalPosition;
+
     void Start()
     {
         finishLine.SetActive(false);
-        UpdateTutorialText();
+        tutorialRectTransform = tutorialText.GetComponent<RectTransform>();
+        originalPosition = tutorialRectTransform.anchoredPosition;
+        StartCoroutine(AnimateText("Use WASD to steer."));
     }
 
     void Update()
@@ -22,44 +28,49 @@ public class DrivingTutorial : MonoBehaviour
         if (!hasSteered && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
         {
             hasSteered = true;
-            UpdateTutorialText();
+            StartCoroutine(AnimateText("Press Space to brake."));
         }
 
         if (!hasBraked && Input.GetKeyDown(KeyCode.Space))
         {
             hasBraked = true;
-            UpdateTutorialText();
+            StartCoroutine(AnimateText("Press C to change camera."));
         }
 
         if (!hasChangedCam && Input.GetKeyDown(KeyCode.C))
         {
             hasChangedCam = true;
-            UpdateTutorialText();
+            StartCoroutine(AnimateText("Press L to turn on headlights."));
         }
 
         if (!hasTurnedOnLights && Input.GetKeyDown(KeyCode.L))
         {
             hasTurnedOnLights = true;
-            UpdateTutorialText();
-            EnableFinishLine();
+            StartCoroutine(AnimateText("Drive to the finish line to complete the tutorial."));
+            finishLine.SetActive(true);
         }
     }
 
-    void UpdateTutorialText()
+    IEnumerator AnimateText(string message)
     {
-        if (!hasSteered)
-            tutorialText.text = "Use WASD to steer.";
-        else if (!hasBraked)
-            tutorialText.text = "Press Space to brake.";
-        else if (!hasChangedCam)
-            tutorialText.text = "Press C to change camera.";
-        else if (!hasTurnedOnLights)
-            tutorialText.text = "Press L to turn on headlights.";
-    }
+        tutorialText.text = message;
 
-    void EnableFinishLine()
-    {
-        tutorialText.text = "Drive to the finish line to complete the tutorial.";
-        finishLine.SetActive(true);
+        RectTransform rect = tutorialText.GetComponent<RectTransform>();
+        Vector2 startPos = new Vector2(0, Screen.height); // Slide in from top
+        Vector2 endPos = originalPosition;
+
+        float duration = 1f;
+        float elapsed = 0f;
+
+        rect.anchoredPosition = startPos;
+
+        while (elapsed < duration)
+        {
+            rect.anchoredPosition = Vector2.Lerp(startPos, endPos, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        rect.anchoredPosition = endPos;
     }
 }
